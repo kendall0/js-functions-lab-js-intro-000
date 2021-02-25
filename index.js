a472ced43dd3b3f6446a77237a18a652d2d99f64
Kendall George <kendallanngeorge@gmail.com>
2021-02-25 20:13:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,15 +6,3 @@ var name = "you"
 function happyHolidaysTo(name) {
   return(`Happy holidays, ${name}!`);
 }
-
-var holiday = Christmas
-var name = Stan
-function happyCustomHolidayTo(holiday, name) {
-  return(`Happy ${holiday}, ${name}!`);
-}
-
-var holiday = Hanukkah
-var days = two
-function holidayCountdown(holiday, days) {
-  returns (`It's ${days} days until ${holiday}!`):
-}
